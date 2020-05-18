865fc65782a3b6d994cac85aee84b74e0364fed5
Jigar44 <jigarrabadiya403@gmail.com>
2020-05-18 12:32:58 +0530

    unit converter 1

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutterdemo/unit.dart';
 
 import 'category.dart';
+import 'category_tile.dart';
+import 'unit.dart';
 
 final _backgroundColor = Colors.green[100];
 
@@ -12,12 +13,17 @@ final _backgroundColor = Colors.green[100];
 ///
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
-// TODO: Make CategoryRoute a StatefulWidget
-class CategoryRoute extends StatelessWidget {
+class CategoryRoute extends StatefulWidget {
   const CategoryRoute();
 
-  // TODO: Create State object for the CategoryRoute
+  @override
+  _CategoryRouteState createState() => _CategoryRouteState();
+}
 
+class _CategoryRouteState extends State<CategoryRoute> {
+  // TODO: Keep track of a default [Category], and the currently-selected
+  // [Category]
+  final _categories = <Category>[];
   static const _categoryNames = <String>[
     'Length',
     'Area',
@@ -28,25 +34,72 @@ class CategoryRoute extends StatelessWidget {
     'Energy',
     'Currency',
   ];
-
-  static const _baseColors = <Color>[
-    Colors.teal,
-    Colors.orange,
-    Colors.pinkAccent,
-    Colors.blueAccent,
-    Colors.yellow,
-    Colors.greenAccent,
-    Colors.purpleAccent,
-    Colors.red,
+  static const _baseColors = <ColorSwatch>[
+    ColorSwatch(0xFF6AB7A8, {
+      'highlight': Color(0xFF6AB7A8),
+      'splash': Color(0xFF0ABC9B),
+    }),
+    ColorSwatch(0xFFFFD28E, {
+      'highlight': Color(0xFFFFD28E),
+      'splash': Color(0xFFFFA41C),
+    }),
+    ColorSwatch(0xFFFFB7DE, {
+      'highlight': Color(0xFFFFB7DE),
+      'splash': Color(0xFFF94CBF),
+    }),
+    ColorSwatch(0xFF8899A8, {
+      'highlight': Color(0xFF8899A8),
+      'splash': Color(0xFFA9CAE8),
+    }),
+    ColorSwatch(0xFFEAD37E, {
+      'highlight': Color(0xFFEAD37E),
+      'splash': Color(0xFFFFE070),
+    }),
+    ColorSwatch(0xFF81A56F, {
+      'highlight': Color(0xFF81A56F),
+      'splash': Color(0xFF7CC159),
+    }),
+    ColorSwatch(0xFFD7C0E2, {
+      'highlight': Color(0xFFD7C0E2),
+      'splash': Color(0xFFCA90E5),
+    }),
+    ColorSwatch(0xFFCE9A9A, {
+      'highlight': Color(0xFFCE9A9A),
+      'splash': Color(0xFFF94D56),
+      'error': Color(0xFF912D2D),
+    }),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: Set the default [Category] for the unit converter that opens
+    for (var i = 0; i < _categoryNames.length; i++) {
+      _categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        iconLocation: Icons.cake,
+        units: _retrieveUnitList(_categoryNames[i]),
+      ));
+    }
+  }
+
+  // TODO: Fill out this function
+  /// Function to call when a [Category] is tapped.
+  void _onCategoryTap(Category category) {}
 
   /// Makes the correct number of rows for the list view.
   ///
   /// For portrait, we use a [ListView].
-  Widget _buildCategoryWidgets(List<Widget> categories) {
+  Widget _buildCategoryWidgets() {
     return ListView.builder(
-      itemBuilder: (BuildContext context, int index) => categories[index],
-      itemCount: categories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return CategoryTile(
+          category: _categories[index],
+          onTap: _onCategoryTap,
+        );
+      },
+      itemCount: _categories.length,
     );
   }
 
@@ -63,26 +116,11 @@ class CategoryRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Instead of re-creating a list of Categories in every build(),
-    // save this as a variable inside the State object and create
-    // the list at initialization (in initState()).
-    // This way, you also don't have to pass in the list of categories to
-    // _buildCategoryWidgets()
-    final categories = <Category>[];
-
-    for (var i = 0; i < _categoryNames.length; i++) {
-      categories.add(Category(
-        name: _categoryNames[i],
-        color: _baseColors[i],
-        iconLocation: Icons.cake,
-        units: _retrieveUnitList(_categoryNames[i]),
-      ));
-    }
-
+    // TODO: Import and use the Backdrop widget
     final listView = Container(
       color: _backgroundColor,
       padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: _buildCategoryWidgets(categories),
+      child: _buildCategoryWidgets(),
     );
 
     final appBar = AppBar(
